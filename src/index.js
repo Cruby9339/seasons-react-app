69a363f3978f8214103b40ade4311be8dc86e948
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-11-08 13:39:50 -0600

    adds updated index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+	constructor(props) {
+		super(props);
 
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
-		(position) => console.log(position)	,
-		(err) => console.log(err)
-	);
+		//THIS IS THE ONLY TIME we do direct assignment to this.state
+		this.state ={
+			lat: null
+		};
 
-	return <div>Hi There!</div>;
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({lat: position.coords.latitude});
+			}	,
+			(err) => console.log(err)
+		);
+	}
+
+	render(){
+
+
+		return <div>Latitude: {this.state.lat} </div>
+	}
 }
+
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
