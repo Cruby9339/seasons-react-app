e5c084e745929341797becbf32166dabb73ba2bb
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-11-09 13:39:22 -0600

    adds completed project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
 
@@ -15,7 +16,7 @@ class App extends React.Component {
 		);
 	}
 
-	render(){
+	renderContent(){
 		if(this.state.errorMessage && !this.state.lat){
 			return <div>Error: {this.state.errorMessage}</div>
 		}
@@ -24,8 +25,16 @@ class App extends React.Component {
 			return <SeasonDisplay lat={this.state.lat}/>
 		}
 
-		return <div>Loading!</div>;
+		return <Spinner message="Please accept the location request"/>;
+	}
 
+
+	render(){
+		return (
+			<div>
+			{this.renderContent()}
+			</div>
+		)
 	}
 }
 
