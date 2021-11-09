afb15e23ef6ddcf675011a81a89b861e57b4fd80
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-11-09 12:40:14 -0600

    adds updates

--- src/index.js
+++ src/index.js
@@ -2,42 +2,17 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
 
-		//THIS IS THE ONLY TIME we do direct assignment to this.state
-		this.state ={
-			lat: null,
-			errorMessage: ''
-		};
-
-
-
+	state = {
+		lat: null,
+		errorMessage: ''
+	}
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			(position) => {
-				this.setState({lat: position.coords.latitude});
-			}	,
-			(err) => {
-				this.setState({errorMessage: err.message})
-
-			}
+			position => this.setState({lat: position.coords.latitude}),
+			  err => this.setState({errorMessage: err.message})
 		);
 	}
-	// state = {
-	// 	lat: null,
-	// 	errorMessage: ''
-	// }
-	// componentDidMount() {
-	// 	window.navigator.geolocation.getCurrentPosition(
-	// 		(position) => {
-	// 			this.setState({lat: position.coords.latitude});
-	// 		}	,
-	// 		(err) => {
-	// 			this.setState({errorMessage: err.message})
-	//
-	// 		}
-	// 	);
-	// }
 
 	render(){
 		if(this.state.errorMessage && !this.state.lat){
